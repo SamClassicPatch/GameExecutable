5e59122022a0b2935d981baf0a3d87a1db199597
Dreamy Cecil <stalker27production@gmail.com>
2022-06-22 23:10:54 +0300

    Identify native screen resolution in the resolution list.

--- GUI/Menus/MenuActions.cpp
+++ GUI/Menus/MenuActions.cpp
@@ -43,16 +43,25 @@ static CDisplayMode *_admResolutionModes = NULL;
 // make description for a given resolution
 static CTString GetResolutionDescription(CDisplayMode &dm) {
   CTString str;
+
   // if dual head
   if (dm.IsDualHead()) {
     str.PrintF(TRANS("%dx%d double"), dm.dm_pixSizeI / 2, dm.dm_pixSizeJ);
-    // if widescreen
+
+  // if widescreen
   } else if (dm.IsWideScreen()) {
     str.PrintF(TRANS("%dx%d wide"), dm.dm_pixSizeI, dm.dm_pixSizeJ);
-    // otherwise it is normal
+
+  // otherwise it is normal
   } else {
     str.PrintF("%dx%d", dm.dm_pixSizeI, dm.dm_pixSizeJ);
   }
+
+  // [Cecil] Matching the screen
+  if (PIX2D(dm.dm_pixSizeI, dm.dm_pixSizeJ) == _vpixScreenRes) {
+    str += TRANS(" (Native)");
+  }
+
   return str;
 }
 
