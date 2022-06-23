2741d5648cc0330cb3693490e275d4ab250bf65f
Dreamy Cecil <stalker27production@gmail.com>
2022-06-23 03:01:48 +0300

    Implement custom command registry for storing values of new commands. They're being saved and restored from the "Scripts/CustomSymbols.ini" config file.

--- Cecil/CecilExtensions.cpp
+++ Cecil/CecilExtensions.cpp
@@ -15,10 +15,72 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 
 #include "StdH.h"
 
+#include "CecilExtensions.h"
+
 // Render game in the background while in menu
 INDEX sam_bBackgroundGameRender = TRUE;
 
+// Custom command registy
+static CDynamicContainer<CShellSymbol> _cCustomSymbols;
+
+#define CUSTOM_SYMBOLS_CONFIG "Scripts\\CustomSymbols.ini"
+
+// Register a certain command after its change
+static void CECIL_RegisterCommand(void *pCommand) {
+  CShellSymbol *pss = NULL;
+
+  for (INDEX i = 0; i < _pShell->sh_assSymbols.Count(); i++) {
+    CShellSymbol *pssCheck = _pShell->sh_assSymbols.Pointer(i);
+
+    // Matching command
+    if (pssCheck->ss_pvValue == pCommand) {
+      pss = pssCheck;
+      break;
+    }
+  }
+
+  if (pss == NULL) {
+    CPrintF("Could not find corresponding command in the shell for registering!\n");
+    return;
+  }
+
+  // Add symbols to the list
+  if (!_cCustomSymbols.IsMember(pss)) {
+    _cCustomSymbols.Add(pss);
+  }
+
+  // Save symbol values
+  try {
+    CTFileStream strm;
+    strm.Create_t(CTFILENAME(CUSTOM_SYMBOLS_CONFIG));
+
+    FOREACHINDYNAMICCONTAINER(_cCustomSymbols, CShellSymbol, itss) {
+      CShellSymbol *pssWrite = itss;
+
+      // Symbol name
+      strm.FPrintF_t("%s = ", pssWrite->ss_strName);
+
+      // Symbol value
+      strm.FPrintF_t("%s;\n", _pShell->GetValue(pssWrite->ss_strName));
+    }
+
+    strm.Close();
+
+  } catch (char *strError) {
+    CPrintF("Could not save custom symbols: %s\n", strError);
+  }
+};
+
 // Custom initialization
 void CECIL_Init(void) {
-  _pShell->DeclareSymbol("user INDEX sam_bBackgroundGameRender;", &sam_bBackgroundGameRender);
+  // Command registry
+  _pShell->DeclareSymbol("void CECIL_RegisterCommand(INDEX);", &CECIL_RegisterCommand);
+
+  // Custom symbols
+  {
+    _pShell->DeclareSymbol("user INDEX sam_bBackgroundGameRender post:CECIL_RegisterCommand;", &sam_bBackgroundGameRender);
+  }
+
+  // Restore custom symbol values
+  _pShell->Execute("include \"" CUSTOM_SYMBOLS_CONFIG "\";");
 };
