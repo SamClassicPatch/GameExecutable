7e05305a1089c36e126cfa08360e0603f86311af
Dreamy Cecil <stalker27production@gmail.com>
2022-07-05 05:12:06 +0300

    Move patcher from GameExecutable to CoreLib.

--- Cecil/EnginePatches/Patches.h
+++ Cecil/EnginePatches/Patches.h
@@ -20,8 +20,6 @@ with this program; if not, write to the Free Software Foundation, Inc.,
   #pragma once
 #endif
 
-#include "../Patcher/patcher.h"
-
 // Patcher debug output
 bool &Patch_DebugOutput(void);
 
